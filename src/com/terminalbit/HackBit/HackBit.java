bef8c29464dfa7ebeddaa987ddf1c411af899fca
SnakeByte <lokio.casebstv@gmail.com>
2015-05-17 15:08:52 -0500

    Work on Threads :D

--- src/com/terminalbit/HackBit/HackBit.java
+++ src/com/terminalbit/HackBit/HackBit.java
@@ -4,11 +4,9 @@ import com.terminalbit.HackBit.game.Game;
 import com.terminalbit.HackBit.renderer.HBRender;
  
 public class HackBit {
-	public Game hbsession = new Game();
-	public static HBRender HBRenderer = new HBRender();
+	public static Game hbsession = new Game();
     public static void main(String[] args) {
-        HBRenderer.run();
-        System.out.println("Hello World!");
+    	hbsession.start();
     }
  
 }
--- src/com/terminalbit/HackBit/game/Game.java
+++ src/com/terminalbit/HackBit/game/Game.java
@@ -3,22 +3,56 @@ package com.terminalbit.HackBit.game;
 import com.terminalbit.HackBit.game.connection.Connection;
 import com.terminalbit.HackBit.game.render.StateRenderer;
 
-public class Game {
-	public static StateRenderer renderer;
-	public static Connection serverConnection;
-	public static ComputerRoom currentRoom;
-	public static String connectionIP = "fakeServer";
+public class Game implements Runnable {
+	public StateRenderer renderer;
+	public Connection serverConnection;
+	public ComputerRoom currentRoom;
+	public String connectionIP = "fakeServer";
+	public Thread thread;
+	public Boolean alive = false;
 	public Game() {
 		renderer = new StateRenderer();
 		serverConnection = new Connection(connectionIP);
 	}
-	public static void update() {
+	public synchronized void start() {
+		if(alive)
+			return;
+		alive = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+	public synchronized void stop() {
+		if(!alive)
+			return;
+		alive = false;
+		try{
+			System.out.println("Killed the thread.");
+			thread.join();
+			//Kill the thread, guys. :D
+		}catch(InterruptedException e){
+			//print le errors :(
+			e.printStackTrace();
+		}
+	}
+	public void update() {
 		
 	}
-	public static void render() {
-		
+	public void render() {
+		//rendering = later
 	}
-	public static String getIP(int ipID) {
+	public String getIP(int ipID) {
 		return serverConnection.getIP(ipID).get();
+	}
+	@Override
+	public void run() {
+		while(alive){
+			loop();
+		}
+		stop();
+	}
+	private void loop() {
+
+		this.render();
+		this.update();
 	}
 }
